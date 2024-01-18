4fffa6773dc44dcc78e782e6b74483e43b60a116
Danilo Andjelic <daniloandjelici9@gmail.com>
2024-01-18 22:37:23 +0100

    WIP: Add typeorm config and docker to the project

--- tickets-api/src/app.module.ts
+++ tickets-api/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeOrmConfigFactory } from "typeorm.config";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfigFactory,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ tickets-api/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { join } from "path";
+import { User } from "src/users/entities/user.entity";
+
+export function typeOrmConfigFactory(): TypeOrmModuleOptions {
+  return {
+    type: "postgres",
+    host: "postgres",
+    port: 5432,
+    username: "dev",
+    password: "dev",
+    database: "ticket-db",
+    entities: [User],
+    synchronize: true,
+    migrationsRun: true,
+    migrations: [join(__dirname, "migrations/**/*{.ts,.js}")],
+    logging: true,
+  };
+}
