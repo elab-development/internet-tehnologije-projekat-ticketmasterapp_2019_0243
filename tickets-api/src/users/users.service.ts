206be98361b18e2a55ac45c068080408dabecf05
Danilo Andjelic <daniloandjelici9@gmail.com>
2024-01-20 10:13:43 +0100

    Add volume to docker, enable cors and validation pipe

--- tickets-api/src/users/users.service.ts
+++ tickets-api/src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { RegisterUserDto } from "./dto/user.dtos";
-import * as bcrypt from "bcrypt";
+import { hash, genSalt } from "bcrypt";
 import { User } from "./entities/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
@@ -12,21 +12,36 @@ export class UsersService {
     private readonly userRepository: Repository<User>
   ) {}
 
-  async registerUser(createUserDto: RegisterUserDto) {
-    const { email, password, name } = createUserDto;
+  async registerUser(createUserDto: RegisterUserDto): Promise<User> {
+    try {
+      const { email, password, name } = createUserDto;
 
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
+      const existingUser = await this.findUserByEmail(email);
 
-    const [firstName, surname] = name.split(" ");
+      if (existingUser) {
+        throw new ConflictException("Email is already taken");
+      }
 
-    const user = new User();
-    user.email = email;
-    user.password = hashedPassword;
-    user.firstName = firstName;
-    user.surname = surname;
+      const salt = await genSalt();
+      const hashedPassword = await hash(password, salt);
 
-    await this.userRepository.save(user);
+      const [firstName, surname] = name.split(" ");
+
+      const user = new User();
+      user.email = email;
+      user.password = hashedPassword;
+      user.firstName = firstName;
+      user.surname = surname;
+
+      console.log(user);
+
+      await this.userRepository.save(user);
+
+      return user;
+    } catch (error: any) {
+      console.error("Error registering user: ", error.message);
+      throw new Error("Unable to register user");
+    }
   }
 
   findAll() {
