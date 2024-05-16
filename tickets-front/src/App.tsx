daf6e3cb92cd47829928a919cadb4a53f296124c
Danilo Andjelic <daniloandjelici9@gmail.com>
2024-05-16 19:24:40 +0200

    Add replacement for navigation

--- tickets-front/src/App.tsx
+++ tickets-front/src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
+import { Pages } from "../common/common.enums";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState<Pages>(Pages.HOME);
+
+  const navigateToPage = (page: Pages) => {
+    setCurrentPage(page);
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case Pages.HOME:
+        return <div>Home</div>;
+      // return <HomePage />;
+      case Pages.ABOUT:
+        return <div>About</div>;
+      // return <AboutPage />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
       <Header />
